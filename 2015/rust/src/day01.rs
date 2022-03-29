7e01b0e6c6627efe7f422bcb3bdb6280e9ad0020
Daniel Neuhäuser <dasdasich@gmail.com>
2022-03-29 22:06:33 +0200

    add 2015/01 rust

---
+++ 2015/rust/src/day01.rs
@@ -0,0 +1,21 @@
+pub fn main(input: &str) {
+    let mut floor = 0;
+    let mut first_basement_visit_at_position = None;
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '(' => {
+                floor += 1;
+            }
+            ')' => {
+                floor -= 1;
+            }
+            _ => {}
+        }
+        if floor == -1 && first_basement_visit_at_position == None {
+            // The first character in the instructions has position 1, the second character has position 2, and so on.
+            first_basement_visit_at_position = Some(i + 1);
+        }
+    }
+    println!("Floor: {}", floor);
+    println!("First basement visit at position: {:?}", first_basement_visit_at_position);
+}
---
+++ 2015/rust/src/main.rs
@@ -0,0 +1,49 @@
+extern crate clap;
+
+use std::fs::File;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+
+use clap::{ArgEnum, Parser};
+
+mod day01;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(arg_enum)]
+    command: Commands,
+
+    #[clap(default_value = "-")]
+    input: PathBuf,
+}
+
+#[derive(ArgEnum, Clone, Copy, Debug)]
+enum Commands {
+    Day01,
+}
+
+/// Reads the file at `path` into a String. If the `path` is `-`, stdin is read
+/// instead of the file at path `-`.
+fn read_input(path: &Path) -> std::io::Result<String> {
+    let mut buffer = String::new();
+    if let Some("-") = path.to_str() {
+        std::io::stdin().read_to_string(&mut buffer)?;
+    } else {
+        let mut f = File::open(path)?;
+        f.read_to_string(&mut buffer)?;
+    }
+    Ok(buffer)
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Cli::parse();
+    let input = read_input(args.input.as_path())?;
+
+    match &args.command {
+        Commands::Day01 => {
+            day01::main(input.as_str());
+        }
+    }
+
+    Ok(())
+}
